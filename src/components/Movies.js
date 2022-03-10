81585ad1fc99345ca3d604bf01995c3d3dc6594e
SREEVATSAN1252 <sreevatsan1252@gmail.com>
2022-03-10 18:11:13 +0530

    first commit

---
+++ src/components/Movies.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Movies() {
+ return <Container>
+   <h1>
+     Recommended for You
+   </h1>
+   <Content>
+
+     <Wrap>
+      <img src="/images/spiderman.jpg"/> 
+     </Wrap>
+     <Wrap>
+      <img src="/images/spiderman.jpg"/> 
+     </Wrap>
+     <Wrap>
+      <img src="/images/spiderman.jpg"/> 
+     </Wrap>
+     <Wrap>
+      <img src="/images/spiderman.jpg"/> 
+     </Wrap>
+     <Wrap>
+      <img src="/images/spiderman.jpg"/> 
+     </Wrap>
+     <Wrap>
+      <img src="/images/spiderman.jpg"/> 
+     </Wrap>
+     <Wrap>
+      <img src="/images/spiderman.jpg"/> 
+     </Wrap>
+     <Wrap>
+      <img src="/images/spiderman.jpg"/> 
+     </Wrap>
+     
+     
+
+   </Content>
+ </Container>;
+}
+
+export default Movies;
+const Container = styled.div`
+
+`
+const Content = styled.div`
+display:grid;
+grid-gap:25px;
+grid-template-columns:repeat(4,minmax(0,1fr));
+`
+const Wrap = styled.div`
+box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+  rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+img{
+  width:100%;
+  height:100%;
+  object-fit:cover;
+  border-radius:11px;
+  
+  cursor:pointer;
+  
+  
+  &:hover{
+    border:4px solid rgba(249,249,249,0.8);
+    transition-duration:250ms;
+  }
+  
+}
+`
---
+++ src/components/Viewers.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Viewers() {
+ return <Container>
+  <Wrap>
+<img src="/images/viewers-disney.png"/>
+  </Wrap>
+  <Wrap>
+<img src="/images/viewers-marvel.png"/>
+  </Wrap>
+  <Wrap>
+<img src="/images/viewers-national.png"/>
+  </Wrap>
+  <Wrap>
+<img src="/images/viewers-pixar.png"/>
+  </Wrap>
+  <Wrap>
+<img src="/images/viewers-starwars.png"/>
+  </Wrap>
+ </Container>;
+}
+
+export default Viewers;
+const Container = styled.div`
+margin-top:20px;
+display:grid;
+grid-gap:25px;
+grid-template-columns: repeat(5, minmax(0,1fr));
+
+`
+const Wrap = styled.div`
+border:3px solid rgba(249,249,249,0.1);
+border-radius:20px;
+margin-top:20px;
+transition-duration:1500ms;
+box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+img{
+  width:100%;
+  height:100%;
+
+}
+&:hover{
+  border:4px solid rgba(249,249,249,0.8);
+}
+
+`
